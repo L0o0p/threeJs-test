28c02b5ca6646f2c87c308b4133c341ca6acb463
Loop Shen <2947232977@qq.com>
2024-03-14 09:23:17 +0800

    路径修改

--- vite-project/main.js
+++ vite-project/main.js
@@ -3,14 +3,14 @@ import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 // 添加一个环境光
-const light = new THREE.AmbientLight( 0xffffff ); // 柔和的白光
-scene.add( light );
+const light = new THREE.AmbientLight(0xffffff); // 柔和的白光
+scene.add(light);
 
 // 添加鼠标事件监听器
 renderer.domElement.addEventListener('mousemove', onDocumentMouseMove, false);
@@ -19,57 +19,57 @@ renderer.domElement.addEventListener('click', onDocumentMouseClick, false);
 // 根据鼠标在屏幕上的位置来计算一个标准化的二维向量。这个向量将用于Raycaster。
 let mouse = new THREE.Vector2();
 function onDocumentMouseMove(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-    mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
 }
 
 // 使用Raycaster来检测鼠标射线与场景中对象的相交。(二维到三维)
 let raycaster = new THREE.Raycaster();
 
 function onDocumentMouseClick(event) {
-    // 更新鼠标位置
-    onDocumentMouseMove(event);
+  // 更新鼠标位置
+  onDocumentMouseMove(event);
 
-    // 通过摄像机和鼠标位置更新射线
-    raycaster.setFromCamera(mouse, camera);
+  // 通过摄像机和鼠标位置更新射线
+  raycaster.setFromCamera(mouse, camera);
 
-    // 计算物体和射线的交点
-    let intersects = raycaster.intersectObjects(scene.children);
+  // 计算物体和射线的交点
+  let intersects = raycaster.intersectObjects(scene.children);
 
-    if (intersects.length > 0) {
-        // 如果存在交点，intersects[0]是最接近摄像机的交点
-        console.log(intersects[0]);
-        // 例如，你可以选择高亮被点击的物体
-        // intersects[0].object.material.color.set(0xff0000);
-        model = model.rotateY(Math.PI / 4)
-    }
+  if (intersects.length > 0) {
+    // 如果存在交点，intersects[0]是最接近摄像机的交点
+    console.log(intersects[0]);
+    // 例如，你可以选择高亮被点击的物体
+    // intersects[0].object.material.color.set(0xff0000);
+    model = model.rotateY(Math.PI / 4)
+  }
 }
 
 const loader = new GLTFLoader();// 使用外部模型加载器
-let model,modelY; // 定义一个变量来持有您的模型
+let model, modelY; // 定义一个变量来持有您的模型
 // 加载glTF模型
 
-loader.load( './door_body.glb', function ( gltf ) {
-	scene.add( gltf.scene );
+loader.load('../asset/door_body.glb', function (gltf) {
+  scene.add(gltf.scene);
   model = gltf.scene
 
-}, undefined, function ( error ) {
+}, undefined, function (error) {
 
-	console.error( error );
+  console.error(error);
 
-} );
+});
 
-loader.load( './door_frame.glb', function ( gltf ) {
-	scene.add( gltf.scene );
+loader.load('../asset/door_frame.glb', function (gltf) {
+  scene.add(gltf.scene);
   modelY = gltf.scene
 
-}, undefined, function ( error ) {
+}, undefined, function (error) {
 
-	console.error( error );
+  console.error(error);
 
-} );
+});
 
 // const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 // const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
@@ -79,11 +79,11 @@ loader.load( './door_frame.glb', function ( gltf ) {
 camera.position.z = 5;
 
 function animate() {
-	requestAnimationFrame( animate );
-	renderer.render( scene, camera );
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
   // model.rotation.x += 0.01;
   // model.rotation.y += 0.01;
-//   cube.rotation.x += 0.01;
-// cube.rotation.y += 0.01;
+  //   cube.rotation.x += 0.01;
+  // cube.rotation.y += 0.01;
 }
 animate();
